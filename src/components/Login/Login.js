71a6eeef78fcfd44557050e8895c0e3d7146ea0b
Shantanu Kumar <2023mt03501@wilp.bits-pilani.ac.in>
2024-11-16 22:59:55 +0530

    Add files via upload

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,1 +1,50 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../axiosInstance';
 
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosInstance.post('/login', { username, password });
+      if (response.data.message === 'Login successful') {
+        onLogin(response.data);
+        navigate('/home');
+      } else {
+        setError('Invalid credentials');
+      }
+    } catch (error) {
+      setError('Error logging in: ' + 'Invalid credentials');
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
